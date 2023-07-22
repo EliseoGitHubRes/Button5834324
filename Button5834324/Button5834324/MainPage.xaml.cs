f505dbf00712d1e3064ae59e86f8cd5cdccbc86c
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-07-22 11:00:00 -0600

    Agregar archivos de proyecto.

---
+++ Button5834324/Button5834324/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Button5834324
+{
+    public partial class MainPage : ContentPage
+    {
+        ///<Summary>
+        ///<Createddate>21/07/2023</createddate>
+        ///<company>INDEL</company>
+        ///<lastmodificationdate>21/07/2023</lastmodificationdate>
+        ///<lastmodificationdescription>
+        ///PROPIEDADES DEL BUTTON
+        ///</lastmodificationdescription>
+        //////<lastmodifierautor>ELISEO</lastmodifierautor>
+        ///</Summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// EVENTO QUE SE EJECUTA AL PRESIONAR EL BOTÓN
+        /// </summary>
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Presioname de nuevo";
+        }
+        
+    }
+}
